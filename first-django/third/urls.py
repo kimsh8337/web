0ef2c9d2e6b97bfbd2eccd5782b56f14e29dad3a
김석환 <tokany@naver.com>
2020-05-24 22:31:08 +0900

    django make crud

---
+++ first-django/third/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('list/', views.list, name="list"),
+    path('create/', views.create, name="restaurant-create"),
+    path('update/', views.update, name="restaurant-update"),
+    path('detail/', views.detail, name="restaurant-detail"),
+    path('delete/', views.delete, name="restaurant-delete"),
+]
